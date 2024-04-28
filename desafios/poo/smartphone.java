1c298c08244f690b93b939105d72394b01fa7ae7
ntd17 <joao.natividade01@etec.sp.gov.br>
2024-04-27 23:22:29 -0300

    Create smartphone.java

---
+++ desafios/poo/smartphone.java
@@ -0,0 +1,11 @@
+public interface Smartphone {
+    void tocar();
+    void pausar();
+    void selecionarMusica();
+    void ligar();
+    void atender();
+    void iniciarCorreioVoz();
+    void exibirPagina();
+    void adicionarNovaAba();
+    void atualizarPagina();
+}
