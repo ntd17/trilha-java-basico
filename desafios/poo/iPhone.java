88fd1ae23fc4cc243842232c51baf8991624914e
ntd17 <joao.natividade01@etec.sp.gov.br>
2024-04-27 23:24:29 -0300

    Create iPhone.java

---
+++ desafios/poo/iPhone.java
@@ -0,0 +1,29 @@
+public class iPhone implements Smartphone {
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+    public void selecionarMusica() {
+        System.out.println("Música selecionada");
+    }
+    public void ligar() {
+        System.out.println("Calling...");
+    }
+    public void atender() {
+        System.out.println("Chamada atendida.");
+    }
+    public void iniciarCorreioVoz() {
+        System.out.println("Acessando correio de voz, por favor aguarde!");
+    }
+    public void exibirPagina() {
+        System.out.println("Página exibida");
+    }
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada");
+    }
+    public void atualizarPagina() {
+        System.out.println("Página atualizada com sucesso!");
+    }
+}
